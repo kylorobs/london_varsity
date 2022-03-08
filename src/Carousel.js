71ebdcac11aaf9533d33f0254484aceece70fb31
Kyle <kyle@hitpixel.com>
2022-03-08 09:42:30 +0400

    set carousel index

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,55 +1,42 @@
-const Carousel = () => (
-  <div className="max-w-md p-4 space-x-4 carousel carousel-center bg-white rounded-box">
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/Varsity1_ge2q6n"
-        className="rounded-box"
-      />
+import { useEffect, useRef, useState } from 'react';
+
+const imageSrcs = [
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/Varsity1_ge2q6n',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity2_urk9ld',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsiy3_anacdo',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity5_ejg5ay',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity7_ttfufs',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity6_r34nac',
+  'https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity8_mwytva',
+];
+
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(
+    Math.floor(imageSrcs.length / 2)
+  );
+  let carouselEl = useRef(null);
+  const [elements, setElements] = useState([]); // contains HTMLImageElements
+
+  useEffect(() => {
+    console.log('INDEX');
+    console.log(activeIndex);
+    const imgs = carouselEl.querySelectorAll('img');
+    imgs[activeIndex].scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    setElements(imgs);
+  }, []);
+
+  return (
+    <div
+      ref={(el) => (carouselEl = el)}
+      className="max-w-md p-4 space-x-4 carousel carousel-center bg-white rounded-box"
+    >
+      {imageSrcs.map((src) => (
+        <div className="carousel-item">
+          <img alt="" src={src} className="rounded-box" />
+        </div>
+      ))}
     </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity2_urk9ld"
-        className="rounded-box"
-      />
-    </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsiy3_anacdo"
-        className="rounded-box"
-      />
-    </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity5_ejg5ay"
-        className="rounded-box"
-      />
-    </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity7_ttfufs"
-        className="rounded-box"
-      />
-    </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity6_r34nac"
-        className="rounded-box"
-      />
-    </div>
-    <div className="carousel-item">
-      <img
-        alt=""
-        src="https://res.cloudinary.com/kclsu-media/image/upload/c_fill,f_auto,fl_any_format,h_380,w_300/v1/temporary/varsity8_mwytva"
-        className="rounded-box"
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Carousel;
